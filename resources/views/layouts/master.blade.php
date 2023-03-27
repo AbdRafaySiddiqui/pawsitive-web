a5f1fdb3fefba1db35b0f05575ed99207b605f61
Shahmir Sindhu <shahmir@inspedium.com>
2023-03-27 15:33:14 +0500

    found the error for pop up side menu not working

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -598,8 +598,8 @@
 
 
         </script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <!-- <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script> -->
+    {{-- <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> --}}
+    {{-- <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script> --}}
    
 
 
