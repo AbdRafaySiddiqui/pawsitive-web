8241efc300189a3434832b02ae3068913c9f68ba
Shahmir Sindhu <shahmir@inspedium.com>
2023-05-16 17:15:51 +0500

    ss

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -14,7 +14,7 @@
     <link href="{{ asset('public/bower_components/select2/dist/css/select2.min.css')}}" rel="stylesheet">
     <link href="{{ asset('public/bower_components/bootstrap-daterangepicker/daterangepicker.css')}}" rel="stylesheet">
     <link href="{{ asset('public/bower_components/dropzone/dist/dropzone.css')}}" rel="stylesheet">
-    <link href="{{ asset('public/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css')}}" rel="stylesheet">
+    {{-- <link href="{{ asset('public/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css')}}" rel="stylesheet"> --}}
     <link href="{{ asset('public/bower_components/fullcalendar/dist/fullcalendar.min.css')}}" rel="stylesheet">
     <link href="{{ asset('public/bower_components/perfect-scrollbar/css/perfect-scrollbar.min.css')}}" rel="stylesheet">
     <link href="{{ asset('public/bower_components/slick-carousel/slick/slick.css')}}" rel="stylesheet">
