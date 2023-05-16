b3fa35e7bc96615e57ee1b81c7777b0a60511cbc
Shahmir Sindhu <shahmir@inspedium.com>
2023-05-16 18:14:51 +0500

    ss

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -8,8 +8,8 @@
     <meta content="Inspedium Corporation PVT LTD" name="author">
     <meta content="Pawsitiv Web Admin Panel" name="description">
     <meta content="width=device-width, initial-scale=1" name="viewport">
-    <link href="favicon.png" rel="shortcut icon">
-    <link href="apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="{{ asset('public/favicon.png') }}" rel="shortcut icon">
+    {{-- <link href="apple-touch-icon.png" rel="apple-touch-icon"> --}}
     <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500" rel="stylesheet" type="text/css">
     <link href="{{ asset('public/bower_components/select2/dist/css/select2.min.css')}}" rel="stylesheet">
     <link href="{{ asset('public/bower_components/bootstrap-daterangepicker/daterangepicker.css')}}" rel="stylesheet">
