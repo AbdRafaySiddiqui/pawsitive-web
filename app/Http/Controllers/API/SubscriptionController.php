8957ddcf2626f7abb02f466d0e0ada26db9fdf55
Shahmir Sindhu <shahmir@inspedium.com>
2023-05-17 17:38:14 +0500

    made changes to subcription model name

--- app/Http/Controllers/API/SubscriptionController.php
+++ app/Http/Controllers/API/SubscriptionController.php
@@ -4,13 +4,13 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\subscription;
+use App\Models\Subscriptions;
 
 class SubscriptionController extends Controller
 {
     function add(Request $req)
     {
-        $subscription = new subscription;
+        $subscription = new Subscriptions;
         $subscription->name=$req->name;
         $subscription->email=$req->email;
         $subscription->breed=$req->breed;
